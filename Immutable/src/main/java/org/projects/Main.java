f81f80c57a542f86e57a9fec4a1f570592c12b06
PorterZhang2021 <PorterZhang2021@outlook.com>
2025-05-14 13:36:55 +0800

    bug: fix new instance bug

--- Immutable/src/main/java/org/projects/Main.java
+++ Immutable/src/main/java/org/projects/Main.java
@@ -2,9 +2,9 @@ package org.projects;
 
 public class Main {
   public static void main(String[] args) {
-    Person alice = new Person("Alice", "Alaska");
-    Person bob = new Person("Bob", "Boston");
-    Person charlie = new Person("Charlie", "Chicago");
+    Person alice = new Person("Alice", 20);
+    Person bob = new Person("Bob", 30);
+    Person charlie = new Person("Charlie", 40);
     new PrintPersonThread(alice).start();
     new PrintPersonThread(bob).start();
     new PrintPersonThread(charlie).start();
